962793b828a2b93d43ceffb9fe5106a076aa115f
Jason Ma <jason@peartherapeutics.com>
2017-05-24 21:29:03 -0700

    WIP - pass test cases

--- js/IdleTimer.js
+++ js/IdleTimer.js
@@ -1,6 +1,19 @@
 class IdleTimer {
+  constructor(idleAction, idleTime) {
+    this.idleAction = idleAction;
+    this.idleTime = idleTime;
+
+    setInterval(() => this.handleIdle(), this.idleTime);
+  }
+
+  handleIdle() {
+    if (!this.activity) {
+      this.idleAction();
+    }
+  }
+
   handleActivity() {
-    
+    this.activity = true;
   }
 }
 
