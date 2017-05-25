330154aaa0cf9e9e4cecb9a41cd68104361b7b25
Jason Ma <jason@peartherapeutics.com>
2017-05-24 21:04:56 -0700

    WIP - Initial test descriptions

---
+++ test/IdleTimerSpec.js
@@ -0,0 +1,15 @@
+describe('IdleTimer', () => {
+  describe('when idle time has passed', () => {
+    describe('and there has been no activity', () => {
+      it('should call the passed in idle action', () => {
+
+      });
+    });
+
+    describe('and there has been activity', () => {
+      it('should not call the passed in idle action', () => {
+
+      });
+    });
+  });
+});
